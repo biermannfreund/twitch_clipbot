818de52ac7bf25381b99574eef862797a845bdae
biermannfreund <biermannfreund@gmx.de>
2025-06-06 03:35:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import os
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    code = request.args.get('code')
+    if code:
+        return f'✅ Auth-Code erhalten: {code}'
     return '✅ Clipbot ist online!'
 
 @app.route('/create_clip')
