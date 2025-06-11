5a5b6d9274d74b73f8c556144f081014ffe37051
biermannfreund <biermannfreund@gmx.de>
2025-06-11 21:53:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -33,3 +33,6 @@ def create_clip():
 def root():
     return "✅ Twitch Clipbot läuft!"
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
